dbdcc6f81e28b2cb8ced6720d55fefd4bbf019ba
dixitnavadia <dixit_navadiya@yahoo.co.in>
2025-07-12 14:34:42 +0200

    New Page Modal Mui Implenetation in Progress

--- src/pages/NewPage.jsx
+++ src/pages/NewPage.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import * as React from "react";
 import "../styles/NewPage.css"; // Assuming you have a CSS file for styles
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "black",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 const NewPage = () => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <div className="app">
       <aside className="sidebar">
@@ -22,6 +42,7 @@ const NewPage = () => {
           <header className="header">
             <h2>New Product Page</h2>
           </header>
+          <div className="user-icon">U</div>
         </div>
 
         <main className="main-content">
@@ -35,17 +56,37 @@ const NewPage = () => {
               alt="Product Image"
             />
           </form>
-          
+
           <div className="actions">
-            <button className="save">Save</button>
-            <button className="new-element">Add New Element</button>
+            <div>
+              <Button variant="contained">Save</Button>
+            </div>
+            <div>
+            <Button variant="contained" onClick={handleOpen}>
+              Add New Element
+            </Button>
+            <Modal
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+            >
+              <Box sx={style}>
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                  Text in a modal
+                </Typography>
+                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                  Duis mollis, est non commodo luctus, nisi erat porttitor
+                  ligula.
+                </Typography>
+              </Box>
+            </Modal>
+            </div>
           </div>
         </main>
-        
       </div>
 
       <div className="right-panel">
-        <div className="user-icon">U</div>
         <aside className="properties">
           <h3>Quick Properties</h3>
           <div className="prop-field">Font: Arial</div>
